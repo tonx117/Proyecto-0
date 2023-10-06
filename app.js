3bf3e97bb0bdf52ed6870906f816d03147aa01bf
aquinogonzalo <aquinoalejandro666@gmail.com>
2023-10-06 14:27:06 -0300

    Desfibrilador del proyecto

--- app.js
+++ app.js
@@ -24,7 +24,6 @@ sequelize
 require("ejs");
 
 // Middlewares
-// TODO: Implementar middlewares
 app.use(cors());
 app.use(helmet());
 app.use(morgan("dev"));
@@ -42,7 +41,7 @@ app.use("/", require("./routes/index.routes"));
 // Api Routes
 app.use("/api", require("./api/routes/usuario.routes"));
 
-// TODO: Si la petición no coincide con ninguna de las rutas declaradas, mostrar error 404
+
 app.use((req, res, next) => {
   res.status(404).send("Error 404");
 });
