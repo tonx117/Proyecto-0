275f08c319c78429e1614010cb3d101c9d29749e
aquinogonzalo <aquinoalejandro666@gmail.com>
2023-10-11 02:30:15 -0300

    GOOGLE DRIVE API-Funciona

---
+++ google-services/controllers/drive.controller.js
@@ -0,0 +1,74 @@
+import { fileURLToPath } from 'url';
+import path from "node:path";
+import fs from "fs";
+import { drive } from '../app.google.js';
+
+// config filepath
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const nombreArchivo = "prueba.png"
+export const filePath = path.join(__dirname, `../public/${nombreArchivo}`)
+
+
+//upload file
+async function uploadFile() {
+    try {
+        const response = await drive.files.create({
+            requestBody: {
+                name: 'primerintento.jpg', //este es el nombre con el que se guarda
+                mimeType: 'image/jpg',
+            },
+            media: {
+                mimeType: 'image/jpg',
+                body: fs.createReadStream(filePath),
+            },
+        });
+
+        console.log(response.data);
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+uploadFile();
+
+async function deleteFile() {
+    try {
+        const response = await drive.files.delete({
+            fileId: 'YOUR FILE ID',
+        });
+        console.log(response.data, response.status);
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+// deleteFile();
+
+async function generatePublicUrl() {
+    try {
+        const fileId = 'YOUR FILE ID';
+        await drive.permissions.create({
+            fileId: fileId,
+            requestBody: {
+                role: 'reader',
+                type: 'anyone',
+            },
+        });
+
+        /* 
+        webViewLink: View the file in browser
+        webContentLink: Direct download link 
+        */
+        const result = await drive.files.get({
+            fileId: fileId,
+            fields: 'webViewLink, webContentLink',
+        });
+        console.log(result.data);
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+// generatePublicUrl();
