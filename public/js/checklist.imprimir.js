044436bbca37f3ab63400f6ffe11673f8dcb515e
[Tomas Valdez] <Tomasvaldez02@gmail.com>
2023-10-11 10:01:50 -0300

    corrigiendo errores checklist

--- public/js/checklist.imprimir.js
+++ public/js/checklist.imprimir.js
@@ -3,8 +3,8 @@ document.addEventListener("DOMContentLoaded", () => {
   imprimirBtn.addEventListener("click", () => {
     let headercheck = document.getElementById("headercheck");
     let botoncheck = document.getElementById("botoncheck");
-    botoncheck.style.display = "none";
-    headercheck.style.display = "none";
+    //botoncheck.style.display = "none";
+    //headercheck.style.display = "none";
     window.print();
     headercheck.style.display = "block";
     botoncheck.style.display = "block";
