d50c7775b0aeb76cce94dd97190af716fc2b6e58
[Tomas Valdez] <Tomasvaldez02@gmail.com>
2023-10-11 12:13:29 -0300

    Fix: Correccion de checklist y estilos

--- public/js/checklist.imprimir.js
+++ public/js/checklist.imprimir.js
@@ -3,10 +3,13 @@ document.addEventListener("DOMContentLoaded", () => {
   imprimirBtn.addEventListener("click", () => {
     let headercheck = document.getElementById("headercheck");
     let botoncheck = document.getElementById("botoncheck");
-    //botoncheck.style.display = "none";
-    //headercheck.style.display = "none";
+    botoncheck.style.display = "none";
+    headercheck.style.display = "none";
+    const checks = document.querySelectorAll(".mycheck");
+    checks.forEach((check) => check.classList.remove("mycheck"));
     window.print();
     headercheck.style.display = "block";
     botoncheck.style.display = "block";
+    checks.forEach((check) => check.classList.add("mycheck"));
   });
 });
