896d98293a5e5296faa23fe40df45420c9d8444d
aquinogonzalo <aquinoalejandro666@gmail.com>
2023-10-18 16:36:30 -0300

    AHORA SE ORGANIZAN EN UNA CARPETA

--- google-services/controllers/drive.controller.js
+++ google-services/controllers/drive.controller.js
@@ -10,16 +10,17 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 
-
 //upload file
 export default async function uploadFile(nombre) {
     try {
+        const folderId = "1cDcibvQUoWmeEJxQmI5k4vJ98lOjcUku"
         let nombreArchivo = nombre;
         const filePath = path.join(__dirname, `../public/${nombreArchivo}`)
         const response = await drive.files.create({
             requestBody: {
                 name: `${nombre}`, //este es el nombre con el que se guarda
                 mimeType: 'application/pdf', //para los pdf usamos application/pdf
+                parents: [folderId], // Especifica la carpeta de destino utilizando el ID de la carpeta
             },
             media: {
                 mimeType: 'application/pdf', //para los pdf usamos application/pdf
